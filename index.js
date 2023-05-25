e86fdee055beac9794914e4a989337631f0761fe
kaxada <ekaxada@gmail.com>
2023-05-26 01:19:58 +0300

    implemented linting

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-const express = require('express');
-const {Octokit} = require('@octokit/rest')
-const axios = require('axios');
-require('dotenv').config();
+const express = require("express");
+const { Octokit } = require("@octokit/rest");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 const port = 4040;
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send('<a href="/auth/github">Login with GitHub</a>');
 });
 
-app.get('/auth/github', (req, res) => {
-  const scopes = ['user','repo', 'project','admin:org'];
-  res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.clientID}&redirect_uri=${process.env.REDIRECT_URI}&scope=${scopes.join(',')}`);
+app.get("/auth/github", (req, res) => {
+  const scopes = ["user", "repo", "project", "admin:org"];
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${
+      process.env.clientID
+    }&redirect_uri=${process.env.REDIRECT_URI}&scope=${scopes.join(",")}`
+  );
 });
 
-app.get('/api/auth/github/callback', async (req, res) => {
+app.get("/api/auth/github/callback", async (req, res) => {
   try {
     const { code } = req.query;
 
-    const tokenResponse = await axios.post('https://github.com/login/oauth/access_token', {
-      client_id: process.env.clientID,
-      client_secret: process.env.clientSecret,
-      code
-    }, {
-      headers: {
-        Accept: 'application/json'
+    const tokenResponse = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
+        client_id: process.env.clientID,
+        client_secret: process.env.clientSecret,
+        code,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+        },
       }
-    });
+    );
 
-    const octokit = new Octokit({auth: `${tokenResponse.data.access_token}`})
+    const octokit = new Octokit({ auth: `${tokenResponse.data.access_token}` });
 
     const userRepos = await octokit.repos.listForAuthenticatedUser();
     const repositories = userRepos.data;
@@ -40,8 +48,11 @@ app.get('/api/auth/github/callback', async (req, res) => {
       <form action="/api/repo" method="POST">
         <select id="repository-select">
           ${repositories
-            .map((repo) => `<option value="${repo.full_name}">${repo.full_name}</option>`)
-            .join('')}
+            .map(
+              (repo) =>
+                `<option value="${repo.full_name}">${repo.full_name}</option>`
+            )
+            .join("")}
         </select>
         <button type="button" onclick="addRepository()">Add Repository</button>
         <input type="hidden" name="repositories" id="selected-repositories" value="[]">
@@ -86,41 +97,40 @@ app.get('/api/auth/github/callback', async (req, res) => {
         });
       </script>
     `);
-
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Internal Server Error');
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.post('/api/repo', async (req, res) => {
+app.post("/api/repo", async (req, res) => {
   try {
     const selectedRepositories = JSON.parse(req.body.repositories);
-    console.log('Selected Repositories:', selectedRepositories);
+    console.log("Selected Repositories:", selectedRepositories);
 
     const octokit = new Octokit();
 
     const readmeContents = [];
 
     for (const repo of selectedRepositories) {
-      const [owner, repoName] = repo.split('/');
+      const [owner, repoName] = repo.split("/");
 
       const { data: readme } = await octokit.repos.getContent({
         owner,
         repo: repoName,
-        path: 'DEI.md',
+        path: "DEI.md",
       });
 
       if (readme && readme.content) {
-        const content = Buffer.from(readme.content, 'base64').toString('utf-8');
+        const content = Buffer.from(readme.content, "base64").toString("utf-8");
         readmeContents.push({ repo, content });
       }
     }
 
     res.json(readmeContents);
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).send('Internal Server Error');
+    console.error("Error:", error.message);
+    res.status(500).send("Internal Server Error");
   }
 });
 
