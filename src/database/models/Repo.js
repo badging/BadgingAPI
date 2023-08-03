e4ff871f87998fe8d1d2dffef4b95b13806317ce
kaxada <ekaxada@gmail.com>
2023-08-03 10:46:24 +0300

    added timestamp to repo data to track dates of assignment and updating

--- src/database/models/Repo.js
+++ src/database/models/Repo.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const repoSchema = new mongoose.Schema({
-  githubRepoId: { type: Number, required: true },
-  DEICommitSHA: { type: String, required: true },
-  repoLink: { type: String, required: true },
-  badgeType: { type: String, required: true },
-  attachment: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-});
+const repoSchema = new mongoose.Schema(
+  {
+    githubRepoId: { type: Number, required: true },
+    DEICommitSHA: { type: String, required: true },
+    repoLink: { type: String, required: true },
+    badgeType: { type: String, required: true },
+    attachment: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
 
 const Repo = mongoose.model("Repo", repoSchema);
 
