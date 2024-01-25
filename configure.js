c6abdcc25f17676d6fa4773fe7d49d9eed4b49c5
kaxada <ekaxada@gmail.com>
2024-01-25 17:09:07 -0600

    added more config values

--- configure.js
+++ configure.js
@@ -7,6 +7,7 @@ const fs = require("fs");
       "Please input the fields below to configure your project locally"
     );
     const values = {
+      // database configuration
       db_name: await input({ message: "Your  database name:" }),
       db_user: await input({ message: "Your database user name:" }),
       db_password: await password({
@@ -22,13 +23,36 @@ const fs = require("fs");
         default: "mysql",
       }),
 
-      github_client_ID: await input({
+      // Github OAuth configuration
+      github_auth_client_ID: await input({
         message: "Your personal Github OAuth App Client ID: ",
       }),
-      github_client_secret: await input({
+      github_auth_client_secret: await input({
         message: "Your personal Github OAuth App Client Secret:",
       }),
 
+      // GitHub App configuration
+      github_app_id: await input({
+        message: "Your personal Github App ID: ",
+      }),
+
+      github_app_client_ID: await input({
+        message: "Your personal Github App Client ID: ",
+      }),
+
+      github_app_client_secret: await input({
+        message: "Your personal Github App Client Secret:",
+      }),
+
+      github_app_private_key: await input({
+        message: "Your personal Github App Private Key:",
+      }),
+
+      github_app_webhook_secret: await input({
+        message: "Your personal Github App Webhook Secret:",
+      }),
+
+      // Gitlab OAuth configuration
       gitlab_client_ID: await input({
         message: "Your personal GitLab OAuth App Client ID: ",
       }),
@@ -39,12 +63,28 @@ const fs = require("fs");
         message: "Your personal GitLab OAuth APP redirection URI:",
       }),
 
+      // Augur configuration
       augur_client_secret: await input({
         message: "Your Augur Client Secret: ",
       }),
       port: await input({
         message: "Port that you'd like server to run on: ",
         default: 4040,
+      }),
+
+      // email configuration
+      email_host: await input({
+        message: "Your email host:",
+        default: "Gmail",
+      }),
+
+      email_address: await input({
+        message: "Your email address:",
+      }),
+
+      email_password: await password({
+        message: "Your email app password:",
+        mask: true,
       }),
     };
 
@@ -55,8 +95,14 @@ const fs = require("fs");
         DB_HOST=${values.db_host}
         DB_DIALECT=${values.db_dialect}
 
-        GITHUB_APP_CLIENT_ID=${values.github_client_ID}
-        GITHUB_APP_CLIENT_SECRET=${values.github_client_secret}
+        GITHUB_AUTH_CLIENT_ID=${values.github_auth_client_ID}
+        GITHUB_AUTH_CLIENT_SECRET=${values.github_auth_client_secret}
+
+        GITHUB_APP_ID=${values.github_app_id}
+        GITHUB_APP_CLIENT_ID=${values.github_app_client_ID}
+        GITHUB_APP_CLIENT_SECRET=${values.github_app_client_secret}
+        GITHUB_APP_WEBHOOK_SECRET=${values.github_app_webhook_secret}
+        GITHUB_APP_PRIVATE_KEY=${values.github_app_private_key}
 
         GITLAB_APP_CLIENT_ID=${values.gitlab_client_ID}
         GITLAB_APP_CLIENT_SECRET=${values.gitlab_client_secret}
@@ -64,6 +110,10 @@ const fs = require("fs");
 
         AUGUR_CLIENT_SECRET=${values.augur_client_secret}
         PORT=${values.port}
+
+        EMAIL_HOST=${values.email_host}
+        EMAIL_ADDRESS=${values.email_address}
+        EMAIL_PASSWORD=${values.email_password}
     `;
 
     fs.writeFileSync(".env", envFile);
