6e13d000ea8debd8c9bfbbc87e8a7cf7ce4b21f6
Desmond Obisi <desmondcutest@yahoo.com>
2024-03-22 18:17:17 +0100

    fix: allow to set the server for bash to use for the import

--- upload-data.sh
+++ upload-data.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# Check if IMPORT_EVENTS_URL is set, if not, use default URL
+if [ -z "$IMPORT_EVENTS_URL" ]; then
+  IMPORT_EVENTS_URL="http://localhost:4040/api/import-events"
+fi
+
+# Use curl to upload the file
 curl -X POST \
-  http://localhost:4040/api/import-events \
+  "$IMPORT_EVENTS_URL" \
   -H "Content-Type: multipart/form-data" \
   -F "file=@./uploads/event_sheet.xlsx"
