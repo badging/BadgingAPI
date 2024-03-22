cff2cd2e4173885b345b379c27d695997cc51b4b
Desmond Obisi <desmondcutest@yahoo.com>
2024-03-22 17:56:55 +0100

    fix: changed file name to use snake case

--- upload-data.sh
+++ upload-data.sh
@@ -3,4 +3,4 @@
 curl -X POST \
   http://localhost:4040/api/import-events \
   -H "Content-Type: multipart/form-data" \
-  -F "file=@./uploads/event-sheet.xlsx"
+  -F "file=@./uploads/event_sheet.xlsx"
