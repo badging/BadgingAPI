3510c8d360ddcca94e662b1bd56b32596020caf9
Enock Kasaadha <ekaxada@gmail.com>
2024-11-17 16:00:00 -0600

    remove unused help function and its import from event badging module

--- event_badging/help.js
+++
@@ -1,7 +0,0 @@
-const help = async () => {
-  /*************
-   * this section will be used to post the help message
-   */
-};
-
-module.exports = help;
--- event_badging/index.js
+++ event_badging/index.js
@@ -1,10 +1,8 @@
 const assignChecklist = require("./assignChecklist");
-const help = require("./help");
 const { welcome, saveEvent, getResults, endReview } = require("./eventBadging");
 
 module.exports = {
   welcome,
-  help,
   getResults,
   endReview,
   assignChecklist,
